c6b06d7e4cba5f96cb021ef01f045a05dcea9b1a
makara4code <nuolmakara@gmail.com>
2025-11-08 17:06:59 +0700

    feat: add login with email and password

---
+++ app/login/login-form.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPasswrod] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<any>();
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setLoading(true);
+    const { error } = await authClient.signIn.email({
+      email: email,
+      password: password,
+    });
+    setLoading(false);
+
+    if (error) {
+      setErrors(error);
+      return;
+    }
+
+    router.push("/dashboard");
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            {errors?.message && (
+              <Alert variant="destructive">
+                <AlertTitle>{errors.message}</AlertTitle>
+              </Alert>
+            )}
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  onChange={(e) => setPasswrod(e.target.value)}
+                />
+              </Field>
+              <Field>
+                <Button type="submit" disabled={loading}>
+                  {loading ? "Loading..." : "Login"}
+                </Button>
+                <Button variant="outline" type="button">
+                  Login with Google
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
